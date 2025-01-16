b4da60d11a2a1e130d8532cf6c85765c1d4cd7f0
Muhammad Ameer Hamza <129995036+nexusameer@users.noreply.github.com>
2025-01-16 15:09:39 +0500

    abv

--- Myportfolio/Myportfolio/settings.py
+++ Myportfolio/Myportfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+# import django_heroku
 import dj_database_url
 import os
 from dotenv import load_dotenv
@@ -130,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
